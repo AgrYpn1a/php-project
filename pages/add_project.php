27763c27cab28b28832f8938e7ca269b6799d137
miljans <miljan022@gmail.com>
2019-12-09 19:21:01 +0100

    changes to adding project and addes tasks page

--- pages/add_project.php
+++ pages/add_project.php
@@ -3,37 +3,55 @@
 	if(!isset($_SESSION['name']))
     	header('Location: '.'../index.php');
 
-  $style_sheet_tags_input = '../styles/bootstrap-tagsinput.css'
+  $style_sheet_tags_input = '../styles/bootstrap-tagsinput.css';
+
+  if(!isset($_SESSION['tags']) || $_SESSION['tags'] == NULL)
+    $_SESSION['tags'] = array();
 
   // TODO Handle adding tags from form
+  if(isset($_POST['add_tag'])) {
+    $tag = $_POST['tag_name'];
+
+    if(!in_array($tag, $_SESSION['tags'])) {
+      array_push($_SESSION['tags'], $tag);
+    }
+  }
 
  ?>
 
 <?php include("../templates/header.php") ?>
-<div class="container">
+<div class="container" style="width: 60%; padding-top: 20px; padding-bottom: 20px">
 
 	<form method="POST">
   <div class="form-group">
     <label for="exampleFormControlInput1">Project Name</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Name of your project" required>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name of your project" required>
   </div>
   <div class="form-group">
     <label for="exampleFormControlTextarea1">Project description</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
   </div>
   <label for="exampleFormControlTextarea1">Tags</label>
+  </form>
 
   <ul class="list-group" style="margin-bottom: 10px;">
-    <?php // TODO dynamically list all tags ?>
-    <li class="list-group-item">Tag1</li>
+  <?php foreach ($_SESSION['tags'] as $tag) { ?>    
+    <li class="list-group-item">
+      <?php echo $tag; ?>
+      <span class="float-right"> 
+        <button type="submit" name="remove" class="btn btn-outline-secondary">Remove</button>
+      </span>
+    </li>
+  <?php } ?>
   </ul>
 
-  <div class="input-group">
-      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Tag" required>
-      <span class="input-group-btn">
-        <button type="submit" name="add_tag" class="btn btn-outline-secondary">Add Tag</button>
-      </span>
+  <form method="POST">
+    <div class="input-group">
+        <input type="text" name="tag_name" class="form-control" id="exampleFormControlInput1" placeholder="Tag" required>
+        <span class="input-group-btn">
+          <button type="submit" name="add_tag" class="btn btn-outline-secondary">Add Tag</button>
+        </span>
+      </form>
   </div>
-</form>
 </div>
 <?php include("../templates/footer.php") ?>
---
+++ pages/tasks.php
@@ -0,0 +1,5 @@
+<?php include("../templates/header.php") ?>
+<div class="container" style="padding-top:20%;">
+	<h2 align="center">Task list</h2>
+</div>
+<?php include("../templates/footer.php") ?>
